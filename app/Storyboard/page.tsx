a0cd2c16cf997aaaf4d08eb841e70bf3e354d785
minggichae <che08180@naver.com>
2026-07-06 09:39:33 +0900

    feat: 마크업 추가 #1

--- app/Storyboard/page.tsx
+++ app/Storyboard/page.tsx
@@ -3,10 +3,19 @@ import { storyboardFields } from '@/app/data/storyboardFields';
 
 export default function Storyboard() {
   return (
-    <div>
-      {storyboardFields.map((field) => (
-        <FormField key={field.id} field={field} />
-      ))}
-    </div>
+    <>
+      <div>
+        <span>스토리 보드 설명</span>
+        <div>
+          {storyboardFields.map((field) => (
+            <FormField key={field.id} field={field} />
+          ))}
+        </div>
+        <button>스토리보드 생성하기</button>
+      </div>
+      <div>
+        <div>{/* output 공간 */}</div>
+      </div>
+    </>
   );
 }
