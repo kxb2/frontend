bd69b9c17ef48565c7ee30a8f43da595d09631e1
nomineaquae <yoojin629@gmail.com>
2026-07-10 15:43:26 +0900

    style: 캔버스 화면(배경) UI/디자인 반영 #20

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -3,11 +3,8 @@ import Toolbar from '@/app/canvas/_components/toolbar';
 
 export default function CanvasPage() {
   return (
-    <main className="flex-1 min-h-0 flex flex-col p-4">
-      <p className="mb-4 text-sm text-gray-600 shrink-0">캔버스 창</p>
-      <div className="flex-1 min-h-0">
-        <Canvas />
-      </div>
+    <main className="flex-1 min-h-0">
+      <Canvas />
       <Toolbar />
     </main>
   );
